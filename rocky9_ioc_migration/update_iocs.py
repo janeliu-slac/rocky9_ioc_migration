70582fa2bca7592f4c12b93f45a9293b93dec787
Jane Liu <janeliu@slac.stanford.edu>
2024-11-13 15:26:15 -0800

    Removed trailing whitespace, etc. from pre-commit check in these files.

---
+++ rocky9_ioc_migration/update_iocs.py
@@ -0,0 +1,11 @@
+"""
+This script uses the modules_and_versions.json file to update the
+/configure/RELEASE files in every IOC. Each RELEASE file has
+environmental variables that are module names assigned to their
+latest version. For example,
+
+FFMPEGSERVER_MODULE_VERSION         = R2.1.1-2.2.2
+HISTORY_MODULE_VERSION              = R2.7.0
+IOCADMIN_MODULE_VERSION             = R3.1.16-1.4.0
+
+"""
