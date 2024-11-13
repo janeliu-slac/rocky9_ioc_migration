2e95f8723748d51275d255443da204154ad1f4ca
Jane Liu <janeliu@slac.stanford.edu>
2024-11-13 15:23:11 -0800

    Added script to use github gh to git clone all forked IOC repos into user's working directory.

---
+++ rocky9_ioc_migration/ioc_clone_all.sh
@@ -0,0 +1,9 @@
+# Git clone the forked IOC repos into your local working directory.
+
+
+gh repo list janeliu-slac --limit 2000 | while read -r repo _; do
+  if [[ $repo == janeliu-slac/ioc* ]] ;
+  then
+    gh repo clone "$repo"
+  fi
+done
