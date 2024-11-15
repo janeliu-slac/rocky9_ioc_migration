cf5e660ea1f2cae2b5de0f490207fa1aadde21f0
Jane Liu <janeliu@slac.stanford.edu>
2024-11-15 10:02:21 -0800

    Updated comments in ioc_fork_all.sh.

--- rocky9_ioc_migration/ioc_fork_all.sh
+++ rocky9_ioc_migration/ioc_fork_all.sh
@@ -1,4 +1,7 @@
-# Git fork all IOC repos
+# Git fork all IOC repos. This script may have to be run several times due
+# to limitations on github for how quickly a user can git fork a large
+# number of repos. There are 600+ IOC repos.
+
 
 # Git fork repos that start with "ioc"
 gh repo list pcdshub --limit 2000 | while read -r repo _; do
