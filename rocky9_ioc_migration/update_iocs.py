70488ad9c15b06eb3e09070940767f10570e7a80
Jane Liu <janeliu@slac.stanford.edu>
2024-11-20 14:22:41 -0800

    Fixed issue with parsing strings read from a file.

--- rocky9_ioc_migration/update_iocs.py
+++ rocky9_ioc_migration/update_iocs.py
@@ -10,6 +10,7 @@ IOCADMIN_MODULE_VERSION = R3.1.16-1.4.0
 
 """
 
+import json
 import os
 
 from get_module_versions import get_module_versions
@@ -22,69 +23,67 @@ def update_iocs():
     root = os.path.join(os.path.dirname(__file__), "")
     iocs = []
     module_set = set()
-    env_vars = {}
-    env_var_with_versions = {}
 
     # Create a list of IOCs that have a /configure/RELEASE file
     for item in os.listdir(root):
-        if os.path.isdir(os.path.join(root, item)):
+        if os.path.isdir(os.path.join(root, item)) and "__" not in item:
             subfolder = root + item + "/configure/RELEASE"
             if os.path.exists(subfolder):
                 iocs.append(subfolder)
 
-    # Create a list (set) of module environmental variable names based
-    # on all IOCs in pcdshub.
+    # Create a python set of module environmental variable names retrieved
+    # from all IOCs in pcdshub.
     for filepath in iocs:
         if os.path.isfile(filepath):
             with open(filepath, "r+") as file:
                 for line in file:
+                    line = "".join(line.split(" "))
                     if "_MODULE_VERSION=" in line and not line.startswith("#"):
                         mod_name = line.split("=")[0]
                         # put into a set to avoid duplicates
                         module_set.add(mod_name.strip())
 
-    # Generate a text file with a list of all the module environmental variables
-    # that currently appear in the /configure/RELEASE file of every IOC folder.
-    with open("module_environ_vars.txt", "w") as file:
-        for var in module_set:
-            file.write(f"{var}\n")
+    # Output module environmental variables currently used in all IOCs to a
+    # text file.
+    with open("env_modules.txt", "w") as f:
+        for item in module_set:
+            f.write(str(item) + "\n")
 
     """
-    The rest of the migration can't be scripted because certain module
-    environmental variables and their EPICS module folder equivalents
-    are named differently. For example, 'NORMATIVETYPES_MODULE_VERSION'
-    and 'normativetypescpp'. The total number of module environmental
-    variables that show up in all IOC /configure/RELEASE files is less
-    than 70, so I manually assigned version numbers to the module
-    environmental variables and saved it to module_versions_edited.txt.
+    The next step in the Rocky 9 migration can't be scripted because certain
+    module environmental variables and their EPICS module folder equivalents
+    have slightly different names. For example, 'NORMATIVETYPES_MODULE_VERSION'
+    and 'normativetypescpp' and 'ETHERCAT_MODULE_VERSION' and 'ethercatmc'.
+    In the 600+ IOCs there are about 100 module environmental variables used in
+    /configure/RELEASE files, so I manually assigned version numbers to the
+    module environmental variables and saved it to a JSON file
+    env_vars_versions.json. This JSON file will be used to update all the
+    /configure/RELEASE files in IOCs.
     """
 
-    mod_ver_dict = {}
+    with open("env_vars_versions.json") as file:
+        mod_ver_dict = json.load(file)
 
-    # Create a dictionary based on the manually updated file
-    with open("modules_versions_edited.txt", "r") as file:
-        for line in file:
-            line = "".join(line.split(" "))
-            if "_MODULE_VERSION=" in line:
-                env_var = line.split("=")[0]
-                ver = (line.split("=")[1]).strip()
-                mod_ver_dict[env_var] = ver
-
-    # Update all IOCs with the latest version of EPICS modules
+    # Update all IOCs with the latest EPICS module version numbers
     for item in os.listdir(root):
-        if os.path.isdir(os.path.join(root, item)):
+        if os.path.isdir(os.path.join(root, item)) and "__" not in item:
             subfolder = root + item + "/configure/RELEASE"
+            print(subfolder)
+
             # read the entire file
             with open(subfolder, "r+") as file:
                 data = file.readlines()
-                for i, line in enumerate(data):
+
+                for i, line in enumerate(data.copy()):
                     line = "".join(line.split(" "))
+
                     # look for a module environmental variable
                     if "_MODULE_VERSION=" in line:
                         env_var = line.split("=")[0]
                         if env_var in mod_ver_dict:
                             ver = mod_ver_dict[env_var]
                             data[i] = env_var + " = " + ver
+                            print(data[i])
 
 
 if __name__ == "__main__":
