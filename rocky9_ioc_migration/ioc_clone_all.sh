f6f229d2ad3403514de67d9cd3530aaec7066f53
Jane Liu <janeliu@slac.stanford.edu>
2024-12-16 17:50:54 -0800

    Updated ioc_clone_all.sh to only git clone repos starting with 'ioc-common*'

--- rocky9_ioc_migration/ioc_clone_all.sh
+++ rocky9_ioc_migration/ioc_clone_all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Git clone the forked IOC repos into your local working directory.
+# Git clone the forked common IOC repos into your local working directory.
 
 
 gh repo list janeliu-slac --limit 2000 | while read -r repo _; do
-  if [[ $repo == janeliu-slac/ioc* ]] ;
+  if [[ $repo == janeliu-slac/ioc-common* ]] ;
   then
     gh repo clone "$repo"
   fi
