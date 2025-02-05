e38a6a33655ea5509a352e129b602dfb9f491a66
Jane Liu <janeliu@slac.stanford.edu>
2025-02-05 11:45:38 -0800

    Updated ioc_clone_all.sh to git clone forked iocs 2 directory levels up (in /iocs folder).

--- rocky9_ioc_migration/ioc_clone_all.sh
+++ rocky9_ioc_migration/ioc_clone_all.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-# Git clone the forked common IOC repos into your local working directory.
 
+# Git clone the forked common IOC repos into your local working directory.
 
 gh repo list janeliu-slac --limit 2000 | while read -r repo _; do
   if [[ $repo == janeliu-slac/ioc-common* ]] ;
   then
+    cd ../..
     gh repo clone "$repo"
   fi
 done
