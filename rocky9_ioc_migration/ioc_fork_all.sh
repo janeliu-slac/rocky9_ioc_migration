b516a77d1476603efc306ee2b14241fdef44c38b
Jane Liu <janeliu@slac.stanford.edu>
2025-02-05 11:46:01 -0800

    In ioc_fork_all.sh updated comments for clarity.

--- rocky9_ioc_migration/ioc_fork_all.sh
+++ rocky9_ioc_migration/ioc_fork_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+
 # Git fork all common IOC repos. This script may have to be run several times due
-# to limitations on github for how quickly a user can git fork a large
-# number of repos. There are 600+ IOC repos.
+# to limitations in github for how quickly a user can git fork a large
+# number of repos. There are approximately 150 common IOC repos.
 
-
-# Git fork repos that start with "ioc"
+# Git fork repos that start with "ioc-common"
 gh repo list pcdshub --limit 2000 | while read -r repo _; do
   if [[ $repo == pcdshub/ioc-common* ]] ;
   then
