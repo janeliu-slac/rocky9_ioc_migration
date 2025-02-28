bf1a08d77e5e5716b57f6b8e81dc57bdde056065
Jane Liu <janeliu@slac.stanford.edu>
2025-02-28 10:26:52 -0800

    Updated ioc_make_all.sh to run 'make' and open a pr when the IOC builds successfully.

--- rocky9_ioc_migration/ioc_make_all.sh
+++ rocky9_ioc_migration/ioc_make_all.sh
@@ -1,103 +1,16 @@
 #!/bin/bash
 
-# This script has multiple functions:
-# 1. Git fetch and rebase master for all common IOCs.
-# 2. Checks if configure/RELEASE and RELEASE_SITE files have been modified
-#    in each IOC. If so, git add and commit files and push to remote.
-# 3.
-
 ##############################################################################
 # REQUIRES UPDATE: Give the 'branch_name' variable a new git branch name
 ##############################################################################
 branch_name="rocky9_master"
 
-release_file="RELEASE"
-release_local_file="RELEASE.local"
-release_site_file="RELEASE_SITE"
-
-# Specify the commands to execute
-error_log="$(pwd)/error.log"
-
 # Go up into ../../iocs folder
 cd ../..
 
-##### Copy all changes from master to the new branch ($branch_name)
-##### (Use this if you accidentally make changes to master instead
-##### of the new branch)
-# find . -maxdepth 2 -type d -name .git -exec sh -c "cd \"{}\"/../ && \
-# pwd && \
-# git stash && \
-# git checkout -b $branch_name && \
-# git stash apply && \
-# sleep 2" \;
-
-# #### Git fetch upstream and rebase master
-# for dir in */; do
-#     if [ -d "$dir" ]; then
-#         (
-#             cd "$dir"
-#             pwd
-#             git checkout $branch_name
-#             git stash
-#             git checkout master
-#             git fetch upstream
-#             git rebase upstream/master
-#             sleep 2
-#             git push -f
-#             sleep 2
-#             git checkout $branch_name
-#             git rebase master
-#             git push --set-upstream origin $branch_name -f
-#             git stash pop
-#             cd ..
-#             sleep 3
-#         )
-#     fi
-# done
-
-##### Git add/commit/push configure/RELEASE and RELEASE_SITE files
-for dir in */; do
-    if [ -d "$dir" ]; then
-        (
-            cd "$dir"
-            pwd
-            git checkout $branch_name
-            if [ -f "$release_site_file" ]; then
-                git diff --quiet "$release_site_file"
-                if [ $? -ne 0 ]; then
-                    git add "$release_site_file"
-                    git commit -m "Updated $release_site_file"
-                    echo "Updated $dir $release_site_file."
-                fi
-            fi
-            # if [ -d "configure" ]; then
-            #     cd configure
-            #     if [ -f "$release_file" ]; then
-            #         git diff --quiet "$release_file"
-            #         if [ $? -ne 0 ]; then
-            #             git add "$release_file"
-            #             git commit -m "Updated $release_file"
-            #             echo "Updated $dir configure/$release_file with newest module version numbers."
-            #         fi
-            #     fi
-            #     if [ -f "$release_local_file" ]; then
-            #         git diff --quiet "$release_local_file"
-            #         if [ $? -ne 0 ]; then
-            #             git add "$release_local_file"
-            #             git commit -m "Updated $release_local_file"
-            #             echo "Updated $dir configure/$release_local_file with newest module version numbers."
-            #         fi
-            #     fi
-            #     cd ..
-            # fi
-            # cd ..
-            # git push --set-upstream origin $branch_name
-            sleep 1
-        )
-    fi
-done
-
-# ##### Run 'make' on each IOC and output compilation errors to an error log file
+##### Runs 'make' on all IOCs. If IOC builds without errors, open a PR.
+error_log="error.log"
+touch "$error_log"
 
 # find . -maxdepth 2 -type d -name .git -exec sh -c "cd \"{}\"/../ && \
 # make_distclean && \
@@ -106,4 +19,22 @@ done
 # make 2>> $error_log && \
 # sleep 2" \;
 
-## TO-DO: Open git PR
+for dir in */; do
+    if [ -d "$dir" ]; then
+        (
+            cd "$dir"
+            echo -e "\e[1;35m$dir.\e[0m"
+            git checkout $branch_name
+            make distclean
+            make 2>$error_log
+            if [ $? -ne 0 ]; then
+                echo "An error occurred during make. See error.log for details."
+            else
+                echo "Make completed successfully. Opening a pull request."
+                # gh pr create --head --title "ECS-6549 Build and release IOCs for Rocky 9" --body "Updated configuration files for Rocky 9 migration."
+            fi
+            cd ..
+            sleep 2
+        )
+    fi
+done
