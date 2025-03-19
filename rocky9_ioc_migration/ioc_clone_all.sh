11cbad014fb3ce080ab61d8107fc9335b8074583
Jane Liu <janeliu@slac.stanford.edu>
2025-03-18 17:15:41 -0700

    In ioc_clone_all.sh fixed a bug that prevented git cloning into a folder with the correct IOC name.

--- rocky9_ioc_migration/ioc_clone_all.sh
+++ rocky9_ioc_migration/ioc_clone_all.sh
@@ -4,10 +4,14 @@
 
 # update this with your Github username
 github_username=janeliu-slac
+username_len=${#github_username} # get length of github_username
 
 cd ../..
 
 gh repo list $github_username --limit 2000 | while read -r repo _; do
-  gh repo clone "$repo" "$repo"
-  sleep 2
+  if [[ $repo == $github_username/ioc-common* ]]; then
+    ioc_name="${repo:$username_len}" # truncate the github_username to get IOC repo name and clone it into your dev environment
+    gh repo clone "$repo" "$ioc_name"
+    sleep 1
+  fi
 done
